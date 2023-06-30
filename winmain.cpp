0fdcea85fa47aee1c9049947180afe6f1f81d424
えい 餅桜 <home.ei0408@gmail.com>
2023-06-30 11:21:28 +0900

    最終動作確認

--- winmain.cpp
+++ winmain.cpp
@@ -1,32 +1,26 @@
-﻿//ディレクター：武川隆斗
-#include "DxLib.h"
+﻿#include "DxLib.h"
 
-// プログラムは WinMain から始まります
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
-	//DXライブラリの初期化処理 ここから
-	ChangeWindowMode(TRUE);	//ウィンドウモードにする
-	SetGraphMode(800, 600, 32);	//ウィンドウサイズを設定する
-	if (DxLib_Init() == -1)		// ＤＸライブラリ初期化処理
+	ChangeWindowMode(TRUE);
+	SetGraphMode(800, 600, 32);
+	if (DxLib_Init() == -1)	
 	{
-		return -1;			// エラーが起きたら直ちに終了
+		return -1;
 	}
-	SetDrawScreen(DX_SCREEN_BACK);	//裏画面を描画対象へ
-	//DXライブラリの初期化処理 ここまで
+	SetDrawScreen(DX_SCREEN_BACK);
 
-
-	//メイン処理
 	while (ProcessMessage() == 0 && CheckHitKey(KEY_INPUT_ESCAPE) == 0)
 	{
 		//ドット描画処理
 		int color = GetColor(255, 255, 255);
 		DrawPixel(400, 300, color);
-		ScreenFlip();		//裏画面と表画面の入替
-		ClearDrawScreen();	//裏画面の描画を全て消去
+		ScreenFlip();
+		ClearDrawScreen();
 	}
 
-	DxLib_End();			// ＤＸライブラリ使用の終了処理
+	DxLib_End();
 
-	return 0;				// ソフトの終了 
+	return 0;
 }
 
