85f241e070264e3c4b94afe29efce530c55e1099
えい 餅桜 <home.ei0408@gmail.com>
2023-06-30 11:58:58 +0900

    a

---
+++ _maine.cpp
@@ -0,0 +1,25 @@
+#include "DxLib.h"
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	ChangeWindowMode(TRUE);
+	SetGraphMode(800, 600, 32);
+	if (DxLib_Init() == -1)
+	{
+		return -1;
+	}
+	SetDrawScreen(DX_SCREEN_BACK);
+
+	while (ProcessMessage() == 0 && CheckHitKey(KEY_INPUT_ESCAPE) == 0)
+	{
+		//ÉhÉbÉgï`âÊèàóù
+		int color = GetColor(255, 255, 255);
+		DrawPixel(400, 300, color);
+		ScreenFlip();
+		ClearDrawScreen();
+	}
+
+	DxLib_End();
+
+	return 0;
+}
